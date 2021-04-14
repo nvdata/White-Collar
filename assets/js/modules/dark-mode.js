5f8fcb3f8de02d30c18da3fef6e57950cc0015b5
adr <senhoraroldo@gmail.com>
2021-04-14 14:33:46 -0300

    alt

--- assets/js/modules/dark-mode.js
+++ assets/js/modules/dark-mode.js
@@ -2,34 +2,30 @@ export default
 
 function darkMode() {
     const icon = document.querySelector("button.dark__mode")
-
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        document.body.classList.add("dark__mode")
-
-    }
-
-
+    const body = document.body
     if (icon) {
+
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches)
+            body.classList.add("dark__mode")
+
+
         const lua = icon.querySelector("ion-icon")
 
         icon.addEventListener("click", changeMode)
 
-        console.log(icon, lua);
-
         function changeMode() {
-            document.body.style.transition = "all .3s ease "
-            document.body.classList.toggle("dark__mode")
+            body.style.transition = "all .3s ease"
+            body.classList.toggle("dark__mode")
 
-            console.log("oi");
-
-            if (document.body.classList.contains("dark__mode")) {
+            if (body.classList.contains("dark__mode")) {
                 lua.name = "sunny"
             } else {
                 lua.name = "moon"
 
             }
+
             setTimeout(() => {
-                document.body.style.transition = "initial"
+                body.style.transition = "initial"
             }, 300);
 
         }
